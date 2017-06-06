4a281b14b0aec7e23a0d51af97bec632f0fc47c1
Bilal Muhammad <bmuhamm@gmail.com>
2017-06-06 22:46:16 +0700

    Refactor middleware to reside in a single separate file

--- app/app.js
+++ app/app.js
@@ -5,16 +5,7 @@
 var express = require('express');
 var app = express();
 
-var middleware = {
-    requireAuthentication: function(req, res, next) {
-        console.log('Private route hit!');
-        next();
-    },
-    logger: function(req, res, next) {
-        console.log('[' + req.method + '] ' + req.originalUrl + ' : ' + (new Date().toString()) );
-        next();
-    }
-};
+var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
 
